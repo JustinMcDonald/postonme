7399504fcb0265a5be218ed77f029a4be8620bfe
Hazel <hazel@postonme.com>
2012-12-21 23:24:21 -0500

    issue #55

--- common/footer.php
+++ common/footer.php
@@ -1,7 +1,6 @@
 <div id='bottombanner'>
 	<ul>
-		<li>&copy 2012 by <a href='http://www.justingenuity.com/' style='color:#c4c4c4'>JUSTINGENUITY</a></li>
-		<li><img src="../img/signup_icon.png" /></li> 	
+		<!-- <li><img src="../img/blue_icon.png" /></li> -->	
 		<li><a href='contact.php'>Contact Us</a></li>
 		<li><a href='about.php'>About Us</a></li>
 		<li><a href='help.php'>Help</a></li>
@@ -9,6 +8,7 @@
 		<li><a href='suggestions.php'>Suggestion Box</a></li>
 		<li><a href='terms.php'>Terms Of Use</a></li>
 		<li><a href='privacy.php'>Privacy Policy</a></li>
+		<li id="copy">&copy 2012 <a href='http://www.justingenuity.com/' style='color:#c4c4c4'>JUSTINGENUITY</a></li>
 	</ul>
 </div>
 <!--<div style='position:absolute;bottom:2px;font:12px Verdana;color:#c4c4c4;'>CREATED BY <a href='http://www.justingenuity.com/' style='color:#c4c4c4'>JUSTIN MCDONALD</a></div>-->
